1b998887c35d88350b3d1a3c2fb90298ec3858fe
kenchh <ketchupgurke@gmail.com>
2020-05-03 10:39:42 +0200

    Fixed a bug, where mobs sank into the ground after being hit. Changing the mult from 0 -> 0.001 fixed it and is basically 0.

--- src/main/java/me/rey/core/Warriors.java
+++ src/main/java/me/rey/core/Warriors.java
@@ -18,7 +18,6 @@ import me.rey.core.classes.ClassCondition;
 import me.rey.core.classes.ClassType;
 import me.rey.core.classes.abilities.Ability;
 import me.rey.core.classes.abilities.bandit.Blink;
-import me.rey.core.classes.abilities.bandit.HiddenAssault;
 import me.rey.core.classes.abilities.bandit.Leap;
 import me.rey.core.classes.abilities.bandit.Recall;
 import me.rey.core.classes.abilities.bandit.SmokeBomb;
@@ -52,87 +51,87 @@ import me.rey.core.utils.Text;
 
 /*
  * This is the official Warriors plugin
- * 
+ *
  * @author	Rey
  * @version 1.0.0
  * @since	2020-01-01
  */
 public class Warriors extends JavaPlugin {
-	
+
 	// Gui
 	public static GuiHelp guiHelp;
-	
+
 	// Cache
 	public static ArrayList<Ability> abilityCache;
 	public static ArrayList<ClassCondition> classConditions;
 	public static Map<Player, ClassType> userCache;
 	public static Map<Player, HashMap<ClassType, Build>> buildCache;
 	public static PlayerHitCache hitCache;
-	
+
 	PluginManager pm = Bukkit.getServer().getPluginManager();
 	Plugin plugin;
 	static Warriors instance;
-	
+
 	//Config
 	public static boolean deathMessagesEnabled;
-	
+
 	private final Logger logger = getLogger();
 	private SQLManager sql;
-	
-	
+
+
 	/*
 	 * Called whenever the plugin is ENABLED.
 	 */
 	public void onEnable() {
 		this.plugin = this;
 		instance = this;
-		
+
 		loadConfig();
 		initDatabase();
-		
+
 		this.registerCommands();
 		this.registerListeners();
-		
-		
+
+
 		guiHelp = new GuiHelp(this);
-		
+
 		new PlayerRunnableHandler(this);
 
 		buildCache = new HashMap<>();
 		userCache = new HashMap<>();
 		this.initAbilityCache();
 		this.initConditionCache();
-		
-		
+
+
 		registerEnchantments();
-		
+
 		// HUNGER RATES
-		
+
 		deathMessagesEnabled = this.getConfig().getBoolean("kill-death-messages");
 		logger.warning("Search for any errors in CONSOLE, they may be fatal to player gameplay");
 	}
-	
-	
+
+
 	/*
 	 * Called whenever the plugin is DISABLED.
 	 */
 	public void onDisable() {
 		this.plugin = null;
-		
-		
+
+
 		userCache.clear();
 		userCache = null;
-		
+
 		buildCache.clear();
 		buildCache = null;
-	    
-	    
+
+
 	    sql.onDisable();
 	}
-	
+
 	private void initDatabase() {
 		sql = new SQLManager(this);
-		
+
 		Text.log(this, "=====================================");
 		Text.log(this, "");
 		Text.log(this, "&5&lMySQL database connected!");
@@ -140,18 +139,18 @@ public class Warriors extends JavaPlugin {
 		Text.log(this, "");
 		Text.log(this, "=====================================");
 	}
-	
+
 	public SQLManager getSQLManager() {
 		return sql;
 	}
-	
+
 	public PlayerHitCache getHitCache() {
 		if(hitCache == null)
 			hitCache = new PlayerHitCache(this);
 		return hitCache;
 	}
-	
-	
+
+
 	/*
 	 * Register commands
 	 * FORMAT: this.getCommand("cmd").setExecutor(new CommandExecutorClass());
@@ -161,8 +160,8 @@ public class Warriors extends JavaPlugin {
 		this.getCommand("skill").setExecutor(new Skill());
 		this.getCommand("equip").setExecutor(new Equip());
 	}
-	
-	
+
+
 	/*
 	 * Register listeners
 	 * FORMAT: pm.registerEvents(new ListenerClass(), this);
@@ -175,14 +174,14 @@ public class Warriors extends JavaPlugin {
 		pm.registerEvents(new DamageHandlerEvents(), this);
 		pm.registerEvents(new UseSoupEvent(), this);
 	}
-	
+
 	/*
 	 * Return Main class instance
 	 */
 	public static Warriors getInstance() {
 		return instance;
 	}
-	
+
 	/*
 	 * Initialize all ability listeners
 	 */
@@ -196,12 +195,12 @@ public class Warriors extends JavaPlugin {
 				// KNIGHT
 				// BRUTE
 				));
-		
+
 		for(ClassCondition condition : classConditions) {
 			Bukkit.getPluginManager().registerEvents(condition, this);
 		}
 	}
-	
+
 	/*
 	 * Initialize all ability listeners
 	 */
@@ -209,7 +208,7 @@ public class Warriors extends JavaPlugin {
 		abilityCache = new ArrayList<>(Arrays.asList(
 				//BANDIT
 				new Blink(),
-				new HiddenAssault(),
+				//new HiddenAssault(),
 				new Leap(),
 				new Recall(),
 				new SmokeBomb(),
