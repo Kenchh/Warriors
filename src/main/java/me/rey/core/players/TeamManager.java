e1005b07165366d5b0f76a9a49e890eddebbfd1c
rey-dev <reymainmail@gmail.com>
2020-05-15 18:47:49 -0700

    Team Manager update

--- src/main/java/me/rey/core/players/TeamManager.java
+++ src/main/java/me/rey/core/players/TeamManager.java
@@ -1,63 +1,64 @@
 package me.rey.core.players;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
 
 public class TeamManager {
 	
-	private static Set<Team> teams = new HashSet<>();
+	private static HashMap<User, PlayerTeam> teams = new HashMap<>();
 	
-	public static Set<Team> getTeams(){
+	public static HashMap<User, PlayerTeam> getTeams(){
 		return teams;
 	}
 	
-	public static Team createTeam(Team team) {
+	public static PlayerTeam createTeam(User self, PlayerTeam team) {
 		if(team == null) return team;
-		teams.add(team);
+		teams.put(self, team);
 		return team;
 	}
 	
-	public static void deleteTeam(Team team) {
-		teams.remove(team);
+	public static void deleteTeam(User user) {
+		teams.remove(user);
 	}
 	
-	public static Team getPlayerTeam(User user) {
-		for(Team t : teams)
-			if(t.hasMember(user))
-				return t;
-		return null;
-	}
-	
-	public static Team setPlayerTeam(Team toJoin, User user) {
-		getPlayerTeam(user).removeMember(user);
-		if(toJoin != null) toJoin.addMember(user);
-		return toJoin;
+	public static PlayerTeam getPlayerTeam(User user) {
+		PlayerTeam team = null;
+		for(User u : teams.keySet())
+			if(u.getUniqueId().equals(user.getUniqueId()))
+				team = teams.get(u);
+		
+		return team == null ? createTeam(user, team) : team;
 	}
 
-	public class Team {
+	public class PlayerTeam {
 		
 		private ArrayList<User> team;
+		private User self;
 		
-		public Team(User... players) {
+		public PlayerTeam(User self, User... teammates) {
 			team = new ArrayList<User>();
+			this.self = self;
 			
-			if(players != null)
-				for(User u : players)
+			if(teammates != null)
+				for(User u : teammates)
 					team.add(u);
+		}
+		
+		public User getSelf() {
+			return self;
 		}
 		
 		public ArrayList<User> getMembers(){
 			return team;
 		}
 		
-		public Team removeMember(User user) {
+		public PlayerTeam removeMember(User user) {
 			if(hasMember(user))
 				this.team.remove(this.getMatchingUser(user));
 			return this;
 		}
 		
-		public Team addMember(User user) {
+		public PlayerTeam addMember(User user) {
 			if(!hasMember(user) && TeamManager.getPlayerTeam(user) == null)
 				this.team.add(user);
 			return this;
