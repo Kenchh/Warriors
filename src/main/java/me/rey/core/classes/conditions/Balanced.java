4d68be6aa90e069d777c0db1547d49b842440eea
rey-dev <reymainmail@gmail.com>
2020-06-13 11:06:51 -0700

    Knight -> Balanced

---
+++ src/main/java/me/rey/core/classes/conditions/Balanced.java
@@ -0,0 +1,42 @@
+package me.rey.core.classes.conditions;
+
+import java.util.Iterator;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import me.rey.core.classes.ClassCondition;
+import me.rey.core.classes.ClassType;
+import me.rey.core.players.User;
+import me.rey.core.utils.UtilBlock;
+
+public class Balanced extends ClassCondition {
+	
+	PotionEffect STRENGTH = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 5, 0);
+	PotionEffect RESISTANCE = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 5, 0);
+
+	public Balanced() {
+		super(ClassType.IRON);
+	}
+
+	@Override
+	protected void execute(User user, Player player) {
+		Iterator<Entity> le = UtilBlock.getEntitiesInCircle(player.getLocation(), 5F).iterator();
+		
+		while(le.hasNext()) {
+			Entity next = le.next();
+			
+			if(!(next instanceof Player)) continue;
+			
+			Player ent = (Player) next;
+			if(user.getTeam().contains(ent)) continue;
+			
+			// GIVE STRENGTH
+			player.addPotionEffect(STRENGTH);
+			player.addPotionEffect(RESISTANCE);
+		}
+	}
+
+}
